a38401ff30afc77cd8ec5093c8f639e5e95154d1
Jude Chinonso <jude.chinonso@theagromall.com.ng>
2021-01-26 10:59:08 +0100

    creare the curreny type column

--- server/database/migrations/20201206213555-create-cohort-class-days.js
+++ server/database/migrations/20201206213555-create-cohort-class-days.js
@@ -1,35 +1,31 @@
-'use strict';
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('CohortClassDays', {
+  up: (queryInterface, Sequelize) =>
+    queryInterface.createTable('CohortClassDays', {
       id: {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.UUID,
       },
       classId: {
-        type: Sequelize.UUID
+        type: Sequelize.UUID,
       },
       courseCohortId: {
-        type: Sequelize.UUID
+        type: Sequelize.UUID,
       },
       date: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       time: {
-        type: Sequelize.TIME
+        type: Sequelize.TIME,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('CohortClassDays');
-  }
+        type: Sequelize.DATE,
+      },
+    }),
+  down: (queryInterface) => queryInterface.dropTable('CohortClassDays'),
 };
