7dfd0534504ee79f84e80c92deb5fb6d2904cc2f
Jude Chinoso <jjchinosoviolet@gmail.com>
2021-02-03 15:18:27 +0100

    get support for safari

--- site/src/components/Files/FileSec/index.js
+++ site/src/components/Files/FileSec/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import delete_icon from '../../../assets/icons/delete.png';
-import numeral from 'numeral';
-import DropDown from '../../DropDown';
-import './style.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import delete_icon from "../../../assets/icons/delete.png";
+import numeral from "numeral";
+import DropDown from "../../DropDown";
+import "./style.scss";
 
 const FileSec = ({ file, personal = false, deleteFile, linkExt }) => {
   const view = async (contentLink) => {
-    window.open(contentLink, '_blank');
+    window.open(contentLink, "_blank");
   };
 
   const download = async (contentLink) => {
@@ -19,57 +19,61 @@ const FileSec = ({ file, personal = false, deleteFile, linkExt }) => {
   };
 
   return (
-    <div className="file_card flex-row j-space">
-      <div
-        className="left flex-row j-start al-start"
-        onClick={() => view(file.webViewLink)}
-      >
-        <img src={file.iconLink} alt="pdf" />
-        <div className="txt-sec">
-          <h2 className="clipped-text" style={{ '--number': 1 }}>
-            {file.name}
-          </h2>
-          {personal ? (
-            <>
-              {file.isGraded ? (
-                <small className="theme-color">Graded</small>
-              ) : (
-                <small className="theme-color">Not Graded</small>
-              )}
-            </>
-          ) : (
-            <small>{numeral(file.size).format('0b')}</small>
-          )}
+    <div className="file_card">
+      <div className="flex-row j-space">
+        <div
+          className="left flex-row j-start al-start"
+          onClick={() => view(file.webViewLink)}
+        >
+          <img src={file.iconLink} alt="pdf" />
+          <div className="txt-sec">
+            <h2 className="clipped-text" style={{ "--number": 1 }}>
+              {file.name}
+            </h2>
+            {personal ? (
+              <>
+                {file.isGraded ? (
+                  <small className="theme-color">Graded</small>
+                ) : (
+                  <small className="theme-color">Not Graded</small>
+                )}
+              </>
+            ) : (
+              <small>{numeral(file.size).format("0b")}</small>
+            )}
+          </div>
         </div>
-      </div>
 
-      {personal ? (
-        <div>
-          {file.isGraded ? (
-            <Link
-              to={`/courses/assignment/${linkExt.courseId}/${linkExt.classroom}/${file.resourceId}`}
-              className="theme-color grade"
-            >
-              <small>View Grade</small>
-            </Link>
-          ) : (
-            <img
-              src={delete_icon}
-              alt="delete"
-              className="img_grade"
-              onClick={() => deleteFile(file.id)}
-            />
-          )}
-        </div>
-      ) : (
-        <DropDown>
-          <ul>
-            <li onClick={() => view(file.webViewLink)}>View</li>
-            <li onClick={() => download(file.webContentLink)}>Download</li>
-            {deleteFile && <li onClick={() => deleteFile(file.id)}>Delete</li>}
-          </ul>
-        </DropDown>
-      )}
+        {personal ? (
+          <div>
+            {file.isGraded ? (
+              <Link
+                to={`/courses/assignment/${linkExt.courseId}/${linkExt.classroom}/${file.resourceId}`}
+                className="theme-color grade"
+              >
+                <small>View Grade</small>
+              </Link>
+            ) : (
+              <img
+                src={delete_icon}
+                alt="delete"
+                className="img_grade"
+                onClick={() => deleteFile(file.id)}
+              />
+            )}
+          </div>
+        ) : (
+          <DropDown>
+            <ul>
+              <li onClick={() => view(file.webViewLink)}>View</li>
+              <li onClick={() => download(file.webContentLink)}>Download</li>
+              {deleteFile && (
+                <li onClick={() => deleteFile(file.id)}>Delete</li>
+              )}
+            </ul>
+          </DropDown>
+        )}
+      </div>
     </div>
   );
 };
