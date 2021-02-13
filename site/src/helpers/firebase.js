2ebf6b547d174306cb5f64a8856abf6a43ca512b
Jude Chinoso <jjchinosoviolet@gmail.com>
2021-02-13 18:07:09 +0100

    add google signin

---
+++ site/src/helpers/firebase.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
+
+googleProvider.addScope('email');
+googleProvider.addScope('profile');
+facebookProvider.addScope('email');
+facebookProvider.addScope('profile');
